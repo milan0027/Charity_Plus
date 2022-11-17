709d21d183d3eda7755d3147eb7f12051ea0c91e
Milan Mundhra <milanmundhra27@gmail.com>
2022-11-17 19:56:51 +0530

    tabs and spaces 2

--- models/Comment.js
+++ models/Comment.js
@@ -1,58 +1,60 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const commentSchema=new Schema({
-    user:{
+const commentSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  post: {
+    type: Schema.Types.ObjectId,
+    ref: "Post",
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+  },
+  image: [
+    {
+      url: String,
+    },
+  ],
+  avatar: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  likes: [
+    {
+      user: {
         type: Schema.Types.ObjectId,
-        ref: 'User'
+        ref: "users",
+      },
     },
-    post:{
+  ],
+  unlikes: [
+    {
+      user: {
         type: Schema.Types.ObjectId,
-        ref: 'Post'
+        ref: "users",
+      },
     },
-    text: {
-        type: String,
-        required: true
-    },
-    name: {
-        type:String
-    },
-    image: [ {
-        url: String
-    }],
-    avatar: {
-        type: String
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    },
-    likes:[
-        {
-            user: {
-                type: Schema.Types.ObjectId,
-                ref: 'users'
-            }
-        }
-    ],
-    unlikes:[
-        {
-            user: {
-                type: Schema.Types.ObjectId,
-                ref: 'users'
-            }
-        }
-    ],
-    approval: {
-        type: Boolean,
-        default: false
-    },
-    post_event: {
-        type: Boolean
-    },
-    user_typeof: {
-        type: Boolean
-    }
+  ],
+  approval: {
+    type: Boolean,
+    default: false,
+  },
+  post_event: {
+    type: Boolean,
+  },
+  user_typeof: {
+    type: Boolean,
+  },
 });
 
-module.exports = Comment = mongoose.model('Comment', commentSchema)
+module.exports = Comment = mongoose.model("Comment", commentSchema);
--- models/Post.js
+++ models/Post.js
@@ -1,53 +1,55 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 const PostSchema = new Schema({
-    user: {
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  event: {
+    type: Boolean,
+    default: false,
+  },
+  likes: [
+    {
+      user: {
         type: Schema.Types.ObjectId,
-        ref: 'User'
+        ref: "User",
+      },
     },
-    text: {
-        type: String,
-        required: true
+  ],
+  unlikes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
     },
-    name: {
-        type:String
+  ],
+  image: [
+    {
+      url: String,
     },
-    avatar: {
-        type: String
+  ],
+  comments: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Comment",
     },
-    event: {
-        type: Boolean,
-        default: false
-    },
-    likes:[
-        {
-            user: {
-                type: Schema.Types.ObjectId,
-                ref: 'User'
-            }
-        }
-    ],
-    unlikes:[
-        {
-            user: {
-                type: Schema.Types.ObjectId,
-                ref: 'User'
-            }
-        }
-    ],
-    image: [ {
-        url: String
-    }],
-    comments: [
-        {
-            type:Schema.Types.ObjectId,
-            ref: 'Comment'
-        }
-    ],
-    date: {
-        type: Date,
-        default: Date.now
-    }
-})
-
-module.exports = Post = mongoose.model('Post', PostSchema)
+  ],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+F;
+module.exports = Post = mongoose.model("Post", PostSchema);
