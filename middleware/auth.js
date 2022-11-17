bf61db86088c17af777d24ebd841af90f150c3f3
Milan Mundhra <milanmundhra27@gmail.com>
2022-11-17 19:59:15 +0530

    formatting middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,24 +1,21 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 //const config = require('config')
 
+module.exports = function (req, res, next) {
+  //get token from the header
+  // console.log(req.body, "--from auth.js")
+  const token = req.header("x-auth-token");
 
-module.exports = function(req, res, next){
-
-    //get token from the header
-   // console.log(req.body, "--from auth.js")
-    const token = req.header('x-auth-token')
-
-    //check if no token
-    if(!token){
-        return res.status(401).json({ msg:'no token, authorization denied' })
-    }
-    //verify token
-    try{
-        const decoded = jwt.verify(token, process.env.jwtSecret)
-        req.user = decoded.user
-        next()
-    }catch(err){
-        res.status(401).json({msg: 'token in not valid'})
-
-    }
-}
+  //check if no token
+  if (!token) {
+    return res.status(401).json({ msg: "no token, authorization denied" });
+  }
+  //verify token
+  try {
+    const decoded = jwt.verify(token, process.env.jwtSecret);
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: "token in not valid" });
+  }
+};
